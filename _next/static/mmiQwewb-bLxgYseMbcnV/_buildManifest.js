4758c643d686ab132876ac7e7a97721d3d43554d
juji <jujiyangasli@gmail.com>
2021-06-04 01:59:48 +0700

    Updates

---
+++ _next/static/mmiQwewb-bLxgYseMbcnV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-fc0e0a3832df7c558110.js","static/chunks/pages/index-9841522770720869ebcd.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
